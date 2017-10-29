b9b6cd2301546bdc59e6885b75434be6768fda72
Shea Newton <shnewto@gmail.com>
2017-10-29 14:54:23 -0700

    propigate previous commit's README changes to lib.rs documentation

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //! ## What does a parsable BNF grammar look like?
 //! 
 //! The following grammar from the [Wikipedia page on Backus-Naur form](https://en.wikipedia.org/wiki/Backus%E2%80%93Naur_form#Example)
-//! exemplifies a compatible grammar after adding ';' characters to indicate the end of a producion.
+//! exemplifies a compatible grammar after adding ';' characters to indicate the end of each producion.
 //! 
 //! ```text
 //! <postal-address> ::= <name-part> <street-address> <zip-part>;

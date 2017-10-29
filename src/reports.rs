39fc492b576303df3d22e3e6b0c4498774815a95
Jeff Crocker <crockagile@gmail.com>
2017-10-29 15:20:16 -0700

    Format with rustfmt

--- src/reports.rs
+++ src/reports.rs
@@ -5,20 +5,28 @@ pub fn report_error(err: Err<&[u8], u32>) {
     match err {
         Err::Code(_) => {
             println!("Parsing error: Unknown origin");
-        },
+        }
         Err::Node(_, n) => {
             println!("Parsing error: Unknown origin");
-            for e in n { report_error(e); }
-        },
-        Err::Position(_,p) => {
-            println!("Parsing error: When input is {:?}",
-            String::from_utf8_lossy(p).into_owned());
-        },
-        Err::NodePosition(_,p,n) => {
-            println!("Parsing error: When input is {:?}",
-            String::from_utf8_lossy(p).into_owned());
-            for e in n { report_error(e); }
-        },
+            for e in n {
+                report_error(e);
+            }
+        }
+        Err::Position(_, p) => {
+            println!(
+                "Parsing error: When input is {:?}",
+                String::from_utf8_lossy(p).into_owned()
+            );
+        }
+        Err::NodePosition(_, p, n) => {
+            println!(
+                "Parsing error: When input is {:?}",
+                String::from_utf8_lossy(p).into_owned()
+            );
+            for e in n {
+                report_error(e);
+            }
+        }
     }
 }
 
@@ -27,12 +35,17 @@ pub fn report_incomplete(needed: Needed, actual: usize) {
         Needed::Unknown => {
             println!(
                 "Data error: insufficient size, expectation unknown, \
-                found {:?} bytes", actual);
-        },
+                 found {:?} bytes",
+                actual
+            );
+        }
         Needed::Size(s) => {
             println!(
                 "Data error: insufficient size, expected {:?} bytes \
-                but found {:?}", s, actual);
-        },
+                 but found {:?}",
+                s,
+                actual
+            );
+        }
     }
 }

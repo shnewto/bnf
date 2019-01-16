2ed7b0233789147051db630f0360cf21d427e41d
Shea Newton <shnewto@gmail.com>
2019-01-16 08:43:11 -0800

    Doc formatting and typo fixes. Remove const name case warning

--- src/lib.rs
+++ src/lib.rs
@@ -13,16 +13,16 @@
 //! to indicate the end of a producion)
 //!
 //! ```text
-//! <postal-address> ::= <name-part> <street-address> <zip-part>
+//!  <postal-address> ::= <name-part> <street-address> <zip-part>
 //!
-//!         <name-part> ::= <personal-part> <last-name> <opt-suffix-part> <EOL>
+//!       <name-part> ::= <personal-part> <last-name> <opt-suffix-part> <EOL>
 //!                     | <personal-part> <name-part>
 //!
-//!     <personal-part> ::= <initial> "." | <first-name>
+//!   <personal-part> ::= <initial> "." | <first-name>
 //!
-//!     <street-address> ::= <house-num> <street-name> <opt-apt-num> <EOL>
+//!  <street-address> ::= <house-num> <street-name> <opt-apt-num> <EOL>
 //!
-//!         <zip-part> ::= <town-name> "," <state-code> <ZIP-code> <EOL>
+//!        <zip-part> ::= <town-name> "," <state-code> <ZIP-code> <EOL>
 //!
 //! <opt-suffix-part> ::= "Sr." | "Jr." | <roman-numeral> | ""
 //!     <opt-apt-num> ::= <apt-num> | ""

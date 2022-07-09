c60a790d8bfdb8b92f146b46429b25df9a225f25
Jeffrey Crocker <crockagile@gmail.com>
2022-07-09 17:33:47 -0400

    public parse tree docs (#101)

--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,6 @@ mod production;
 mod term;
 pub use crate::error::Error;
 pub use crate::expression::Expression;
-pub use crate::grammar::Grammar;
+pub use crate::grammar::{Grammar, ParseTree};
 pub use crate::production::Production;
 pub use crate::term::Term;

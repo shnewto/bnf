8b7d754cfd695fae9a40a2171933cbaa172f00db
Axiomatic-Mind <108493465+Axiomatic-Mind@users.noreply.github.com>
2022-09-11 01:46:57 -0700

    Expose ParseTreeNode
    Exposes `ParseTreeNode` to allow full matching of `ParseTree`s

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,6 @@ mod slice_iter;
 mod term;
 pub use crate::error::Error;
 pub use crate::expression::Expression;
-pub use crate::grammar::{Grammar, ParseTree};
+pub use crate::grammar::{Grammar, ParseTree, ParseTreeNode};
 pub use crate::production::Production;
 pub use crate::term::Term;

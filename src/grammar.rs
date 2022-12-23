02d705bb8cfab57f1c4f411c55fed43bb619c696
Jeff Crocker <crockagile@gmail.com>
2022-12-23 11:34:13 -0500

    remove test logging

--- src/grammar.rs
+++ src/grammar.rs
@@ -687,7 +687,6 @@ mod tests {
 
     #[test]
     fn shared_nonterminal_failure() {
-        crate::tracing::init_subscriber();
         let grammar = "
         <start> ::= <shortfail> | <longsuccess>
         <shortfail> ::= <char> 'never'
@@ -710,7 +709,6 @@ mod tests {
 
     #[test]
     fn swap_left_right_recursion() {
-        crate::tracing::init_subscriber();
         let input = "aa a";
 
         let left_recursive: &str = "
